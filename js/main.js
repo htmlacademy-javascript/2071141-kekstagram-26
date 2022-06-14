7994070ba278e039d09ee73b81e08a8723c6abc1
Петр <kravchenko.pb.13@gmail.com>
2022-06-14 21:00:39 +0300

    переписал функции в стрелочные, сделал функцию глаголом и исправил вторую функцию

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
-const randomInteger = function (min, max) {
+const getRandomInteger = (min = 0, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (max > min) {
     return Math.floor(Math.random() * (max - min)) + min;
   } else {return false;}
 };
-randomInteger(3,200);
+getRandomInteger();
 
 /*За основу взят материал из https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random*/
 
-function maxStringLength(stringToTest, maxLength) {
-  if (stringToTest.length <= maxLength) {
-    return true;
-  } else {return false;}
-}
-maxStringLength('Проверка', 6);
+const getMaxStringLength = (stringToTest, maxLength) => stringToTest.length <= maxLength;
+getMaxStringLength();
